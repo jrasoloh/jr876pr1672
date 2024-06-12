d80456b2f66a2a48e76a8c1c13ebd72cbbe33f8a
jossy.rasoloharijaona <jossy.rasoloharijaona@openclassrooms.com>
2024-06-12 16:24:17 +0200

    Enabling oeuvre submission

--- traitement.php
+++ traitement.php
@@ -1,12 +1,28 @@
 <?php
+include 'bdd.php';
 
 if ( empty($_POST['titre']) || empty($_POST['artiste']) || empty($_POST['image']) || empty($_POST['description'])
     || strlen($_POST['description']) < 3
     || !filter_var($_POST['image'], FILTER_VALIDATE_URL)
 ) {
-    header('Location : ajouter.php');
+    header('Location: ajouter.php');
 }
 else {
-    // rajouter a la base de donnees
+
+    $titre = htmlspecialchars($_POST['titre']);
+    $description = htmlspecialchars($_POST['description']);
+    $artiste = htmlspecialchars($_POST['artiste']);
+    $image = htmlspecialchars($_POST['image']);
+    $mysqlClient = connexion();
+    $sqlQuery = "INSERT INTO oeuvres(titre, description, artiste, image) VALUES (:titre, :description, :artiste, :image) ";
+    $ajoutOeuvre = $mysqlClient->prepare($sqlQuery);
+    $ajoutOeuvre->execute([
+        "titre" => $titre,
+        "description" => $description,
+        "artiste" => $artiste,
+        "image" => $image
+    ]);
+
+    header( 'Location: oeuvre.php?id='.$mysqlClient->lastInsertId());
 }
 
